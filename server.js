184fe6ac536a298cd14d109cc977d424547cf6e0
Ulugbek Aripov <ulugbeks@gmail.com>
2018-02-17 11:23:14 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs')
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log+'\n',(err) => {
+    if (err) {
+        console.log('unable to append');
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase ();
+});
+
+
+app.get('/', (req, res) => {
+  res.render('index.hbs',{
+    pageTitle: 'Welcome to this website !'
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About 200 page'
+  });
+});
+
+app.get('/error', (req, res) => {
+  res.send('ERROR');
+});
+
+
+app.listen(3000);
